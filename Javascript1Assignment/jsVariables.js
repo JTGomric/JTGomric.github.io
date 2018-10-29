cc0fd023a1269c64f16d737d86d6db8c9d0c3e38
JTGomric <42543273+JTGomric@users.noreply.github.com>
2018-10-29 11:35:36 -0500

    Add files via upload

---
+++ Javascript1Assignment/jsVariables.js
@@ -0,0 +1,18 @@
+var randomElements = new Array();
+
+randomElements[0] = "item1";
+randomElements[1] = "item2";
+randomElements[2] = "item3";
+randomElements[3] = "item4";
+randomElements[4] = "item5";
+randomElements[5] = "item6";
+
+var el1 = document.getElementById("first");
+var el2 = document.getElementById("second");
+var el3 = document.getElementById("third");
+var el4 = document.getElementById("fourth");
+
+el1.textContent = randomElements[Math.floor(Math.random()*6)];
+el2.textContent = randomElements[Math.floor(Math.random()*6)];
+el3.textContent = randomElements[Math.floor(Math.random()*6)];
+el4.textContent = randomElements[Math.floor(Math.random()*6)];
