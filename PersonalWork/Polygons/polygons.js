599fd4a9ff40e1960f10d275dd66d72a2191b805
JTGomric <42543273+JTGomric@users.noreply.github.com>
2018-11-12 20:21:17 -0600

    Add files via upload

---
+++ PersonalWork/Polygons/polygons.js
@@ -0,0 +1,34 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+//ctx.moveTo(0,0);
+//ctx.lineTo(1000,600);
+
+var xCrds = new Array();
+var yCrds = new Array();
+
+ctx.strokeStyle="#0000FF";
+
+var sNum = 36;
+ctx.lineWidth=0.01;
+var angle = 180-(((sNum-2)*180)/sNum);
+var holder = angle;
+
+ctx.translate(500,300);
+for(var i=0;i<sNum;i++)
+{
+  xCrds[i] = Math.cos((angle*Math.PI)/180)*300;
+  yCrds[i] = Math.sin((angle*Math.PI)/180)*300;
+  angle -= holder;
+
+}
+
+for(var i=0;i<sNum;i++)
+{
+
+  for(var z=0;z<sNum;z++)
+  {
+    ctx.moveTo(xCrds[i],yCrds[i]);
+    ctx.lineTo(xCrds[z],yCrds[z]);
+    ctx.stroke();
+  }
+}
