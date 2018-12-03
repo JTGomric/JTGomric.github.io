bd7cf2c17a7a6c74181fd97e2b4154db0b2e234c
JTGomric <42543273+JTGomric@users.noreply.github.com>
2018-12-03 11:59:32 -0600

    Add files via upload

--- PersonalWork/mandelbrot/mandelbrot.js
+++ PersonalWork/mandelbrot/mandelbrot.js
@@ -2,6 +2,7 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext('2d');
 var xr = ctx.canvas.width;
 var yr = ctx.canvas.height;
+var canvasData = ctx.getImageData(0, 0, 800, 800);
 
 var a = 0;
 var b = 0;
@@ -17,8 +18,25 @@ var imagMax = document.getElementById("imagMax");
 var realMin = document.getElementById("realMin");
 var imagMin = document.getElementById("imagMin");
 
-$("#testButton").on("click",iterate(realMax.value,realMin.value,imagMax.value,imagMin.value,80));
-ctx.strokeStyle = "#FF0000";
+
+function drawPixel (x, y, r, g, b, a) {
+    var index = (x + y * 800) * 4;
+
+    canvasData.data[index + 0] = r;
+    canvasData.data[index + 1] = g;
+    canvasData.data[index + 2] = b;
+    canvasData.data[index + 3] = a;
+}
+
+drawPixel(250,250,0,255,0,255);
+drawPixel(250,255,0,255,0,255);
+drawPixel(250,260,0,255,0,255);
+updateCanvas();
+function updateCanvas() {
+    ctx.putImageData(canvasData, 0, 0);
+}
+
+
 
 
 function mapRange (value, a, b, c, d) {
@@ -29,9 +47,9 @@ function mapRange (value, a, b, c, d) {
 }
 
 function iterate(maxX,minX,maxY,minY,maxIterations){
-for(var x = 0; x<canvas.width;x++){
-  for(var y = 0;y<canvas.height;y++){
-
+for(var x = 0; x<800;x++){
+  for(var y = 0;y<800;y++){
+    //console.log(maxX + ", " + minX + ", " + maxY + ", " + minY)
      a = mapRange(x,0,800,-400,400);
      b = mapRange(y,0,800,-400,400);
      a = mapRange(a,-400,400,minX,maxX);
@@ -56,15 +74,22 @@ for(var x = 0; x<canvas.width;x++){
       n++;
     }
 
-    if(n === maxIterations){
-      ctx.moveTo(x,y);
-      ctx.rect(x,y,1,1);
-    }
+
+      drawPixel(x, y, (n*1000)%255, 0, 0, 255);
+
 
 
 
   }
 
 }
-  ctx.stroke();
+  updateCanvas();
 }
+
+
+
+//Draw handler
+//function test() {
+  //ctx.clearRect(0,0,canvas.width,canvas.height);
+
+//}
