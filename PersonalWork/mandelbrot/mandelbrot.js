77ae28020eff2244e11574fa9ce111e0a1d72f57
JTGomric <42543273+JTGomric@users.noreply.github.com>
2018-12-03 20:38:14 -0600

    Add files via upload

--- PersonalWork/mandelbrot/mandelbrot.js
+++ PersonalWork/mandelbrot/mandelbrot.js
@@ -13,11 +13,6 @@ var n = 0;
 var aa = 0;
 var bb = 0;
 
-var realMax = document.getElementById("realMax");
-var imagMax = document.getElementById("imagMax");
-var realMin = document.getElementById("realMin");
-var imagMin = document.getElementById("imagMin");
-
 
 function drawPixel (x, y, r, g, b, a) {
     var index = (x + y * 800) * 4;
@@ -28,10 +23,7 @@ function drawPixel (x, y, r, g, b, a) {
     canvasData.data[index + 3] = a;
 }
 
-drawPixel(250,250,0,255,0,255);
-drawPixel(250,255,0,255,0,255);
-drawPixel(250,260,0,255,0,255);
-updateCanvas();
+
 function updateCanvas() {
     ctx.putImageData(canvasData, 0, 0);
 }
@@ -43,17 +35,43 @@ function mapRange (value, a, b, c, d) {
     // first map value from (a..b) to (0..1)
     value = (value - a) / (b - a);
     // then map it from (0..1) to (c..d) and return it
-    return c + value * (d - c);
+    return c + (value * (d - c));
 }
 
-function iterate(maxX,minX,maxY,minY,maxIterations){
+function rMapRange (value, a, b) {
+    // first map value from (a..b) to (0..1)
+    var c = document.getElementById("realMin").value;
+    var d = document.getElementById("realMax").value;
+    value = (value - a) / (b - a);
+    // then map it from (0..1) to (c..d) and return it
+    return c + (value * (d - c));
+}
+
+function iMapRange (value, a, b) {
+    // first map value from (a..b) to (0..1)
+    var c = document.getElementById("imagMin").value;
+    var d = document.getElementById("imagMax").value;
+    console.log(c*c*c);
+    value = (value - a) / (b - a);
+    // then map it from (0..1) to (c..d) and return it
+    return c + (value * (d - c));
+}
+
+function iterate(maxIterations){
+  //console.log(document.getElementById("realMax").value + ", " + document.getElementById("realMin").value + ", " + document.getElementById("imagMax").value + ", " + document.getElementById("imagMin").value);
+  var rMax = document.getElementById("realMax");
+  var rMin = document.getElementById("realMin");
+  var iMax = document.getElementById("imagMax");
+  var iMin = document.getElementById("imagMin");
+  console.log(iMax*iMax);
 for(var x = 0; x<800;x++){
   for(var y = 0;y<800;y++){
-    //console.log(maxX + ", " + minX + ", " + maxY + ", " + minY)
+
      a = mapRange(x,0,800,-400,400);
-     b = mapRange(y,0,800,-400,400);
-     a = mapRange(a,-400,400,minX,maxX);
-     b = mapRange(b,-400,400,minY,maxY);
+     a = mapRange(a,-400,400,rMin.value,rMax.value);
+     a = mapRange(y,0,800,-400,400);
+     a = mapRange(a,-400,400,iMin.value,iMax.value);
+
      ca = a;
      cb = b;
 
@@ -74,8 +92,11 @@ for(var x = 0; x<800;x++){
       n++;
     }
 
-
-      drawPixel(x, y, (n*1000)%255, 0, 0, 255);
+    if(n != maxIterations)
+      drawPixel(x, y, n*2.55, (n*5)%255,n, 255);
+    else {
+      drawPixel(x, y, 0, 0,0, 255);
+    }
 
 
 
