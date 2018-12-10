f45080cdf9717a907569072f7fc9785429aedcf2
JTGomric <42543273+JTGomric@users.noreply.github.com>
2018-12-10 12:00:50 -0600

    Add files via upload

---
+++ Semester Project/Semester Project/Sierpinski.js
@@ -0,0 +1,125 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext('2d');
+var canvasData = ctx.getImageData(0, 0, 800, 800);
+
+
+var x1 = 400;
+var y1 = 0;
+var x2 = 0;
+var y2 = 800;
+var x3 = 800;
+var y3 = 800;
+
+var pX;
+var pY;
+
+var xCoords = new Array();
+var yCoords = new Array();
+
+drawPixel(x1,y1,0,0,255,255);
+drawPixel(x2,y2,0,0,255,255);
+drawPixel(x3,y3,0,0,255,255);
+
+document.getElementById("iterations").value = 500;
+var timesClicked = 0;
+
+function callIterate(){
+  iterate(Number(document.getElementById("iterations").value));
+  timesClicked += 1;
+  display();
+}
+
+function reset(){
+  ctx.save();
+
+  // Use the identity matrix while clearing the canvas
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Restore the transform
+  ctx.restore();
+
+  for(var i = 0;i<xCoords.length;i++){
+    xCoords.length = 0;
+    yCoords.length = 0;
+  }
+}
+
+var choice;
+var first = false;
+
+
+
+
+
+function display(){
+    for(var i = 0;i<xCoords.length;i++)
+    {
+      drawPixel(Math.round(xCoords[i]),Math.round(yCoords[i]),255,0,0,255);
+    }
+
+    updateCanvas();
+}
+
+function iterate(numIterations){
+  for(var i =0;i<numIterations;i++)
+  {
+      choice = Math.floor(Math.random() * 3);
+
+      if(first == false && choice == 0)
+      {
+        pX = x1;
+        pY = y1;
+        first = true;
+      }
+      else if(first == false && choice == 1)
+      {
+        pX = x2;
+        pY = y2;
+        first = true;
+      }
+      else if(first == false && choice == 2)
+      {
+        pX = x3;
+        pY = y3;
+        first = true;
+      }
+      else if(first == true && choice == 0)
+      {
+        pX = (pX+x1)/2;
+        pY = (pY+y1)/2;
+
+        xCoords[i] = pX;
+        yCoords[i] = pY;
+      }
+      else if(first == true && choice == 1)
+      {
+        pX = (pX+x2)/2;
+        pY = (pY+y2)/2;
+
+        xCoords[i] = pX;
+        yCoords[i] = pY;
+      }
+      else if(first == true && choice == 2)
+      {
+        pX = (pX+x3)/2;
+        pY = (pY+y3)/2;
+
+        xCoords[i] = pX;
+        yCoords[i] = pY;
+      }
+   }
+}
+
+function updateCanvas() {
+    ctx.putImageData(canvasData, 0, 0);
+}
+
+function drawPixel (x, y, r, g, b, a) {
+    var index = (x + y * 800) * 4;
+
+    canvasData.data[index + 0] = r;
+    canvasData.data[index + 1] = g;
+    canvasData.data[index + 2] = b;
+    canvasData.data[index + 3] = a;
+}
---
+++ Semester Project/Semester Project/mandelbrot.js
@@ -0,0 +1,137 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext('2d');
+var xr = ctx.canvas.width;
+var yr = ctx.canvas.height;
+var canvasData = ctx.getImageData(0, 0, 800, 800);
+
+var a = 0;
+var b = 0;
+var ca = 0;
+var cb = 0;
+var n = 0;
+
+var aa = 0;
+var bb = 0;
+
+var xDiv = document.getElementById("xCrds");
+var yDiv = document.getElementById("yCrds");
+
+var rMax = document.getElementById("realMax");
+var rMin = document.getElementById("realMin");
+var iMax = document.getElementById("imagMax");
+var iMin = document.getElementById("imagMin");
+var maxIterations = document.getElementById("iterations");
+//Default settings
+rMax.value = "2";
+rMin.value = "-2";
+iMax.value = "2";
+iMin.value = "-2";
+maxIterations.value = "40";
+iterate();
+
+var rect = canvas.getBoundingClientRect();
+
+function getXCrds(e){
+  xDiv.textContent = "Real Coordinate: " + mapRange(mapRange((e.clientX-rect.left),0,800,-400,400),-400,400,Number(rMin),Number(rMax));
+}
+function getYCrds(e){
+  yDiv.textContent = "Imaginary Coordinate: " + mapRange(mapRange((e.clientY-rect.top),0,800,-400,400),-400,400,Number(iMax),Number(iMin));
+}
+
+
+
+
+
+canvas.addEventListener('mousemove',getXCrds,false);
+canvas.addEventListener('mousemove',getYCrds,false);
+
+
+
+
+function drawPixel (x, y, r, g, b, a) {
+    var index = (x + y * 800) * 4;
+
+    canvasData.data[index + 0] = r;
+    canvasData.data[index + 1] = g;
+    canvasData.data[index + 2] = b;
+    canvasData.data[index + 3] = a;
+}
+
+
+function updateCanvas() {
+    ctx.putImageData(canvasData, 0, 0);
+}
+
+
+
+
+function mapRange (value, a, b, c, d) {
+    // first map value from (a..b) to (0..1)
+    value = (value - a) / (b - a);
+    // then map it from (0..1) to (c..d) and return it
+    return c + value * (d - c);
+}
+
+
+
+function iterate(){
+  //console.log(document.getElementById("realMax").value + ", " + document.getElementById("realMin").value + ", " + document.getElementById("imagMax").value + ", " + document.getElementById("imagMin").value);
+  rMax = document.getElementById("realMax").value;
+  rMin = document.getElementById("realMin").value;
+  iMax = document.getElementById("imagMax").value;
+  iMin = document.getElementById("imagMin").value;
+  maxIterations = Number(document.getElementById("iterations").value);
+
+for(var x = 0; x<800;x++){
+  for(var y = 0;y<800;y++){
+
+     a = mapRange(x,0,800,-400,400);
+     b = mapRange(y,0,800,-400,400);
+     a = mapRange(a,-400,400,Number(rMin),Number(rMax));
+     b = mapRange(b,-400,400,Number(iMax),Number(iMin));
+
+     ca = a;
+     cb = b;
+
+     n = 0;
+
+
+    while(n < maxIterations){
+       aa = (a*a)-(b*b);
+       bb = 2*a*b;
+
+
+      a = aa + ca;
+      b = bb + cb;
+
+      if(Math.abs(a+b)>16){
+        break;
+      }
+      n++;
+    }
+
+    /*if(n != maxIterations)
+      drawPixel(x, y, n*2.55, n,(n*5)%255, 255);
+    else {
+      drawPixel(x, y, 0, 0,0, 255);
+    }*/
+    if(n != maxIterations)
+      drawPixel(x, y, mapRange(n,0,maxIterations,0,255), 0,0, 255);
+    else
+      drawPixel(x,y,0,0,0,255);
+
+
+
+  }
+
+}
+  updateCanvas();
+}
+
+
+
+//Draw handler
+//function test() {
+  //ctx.clearRect(0,0,canvas.width,canvas.height);
+
+//}
