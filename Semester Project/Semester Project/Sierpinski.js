a993d9e0a8b8c327a8a9dc839581b2de43ebd445
JTGomric <42543273+JTGomric@users.noreply.github.com>
2018-12-12 12:01:28 -0600

    Add files via upload

--- Semester Project/Semester Project/Sierpinski.js
+++ Semester Project/Semester Project/Sierpinski.js
@@ -3,46 +3,64 @@ var ctx = canvas.getContext('2d');
 var canvasData = ctx.getImageData(0, 0, 800, 800);
 
 
-var x1 = 400;
-var y1 = 0;
-var x2 = 0;
-var y2 = 800;
-var x3 = 800;
-var y3 = 800;
+var xCorners = new Array();
+var yCorners = new Array();
 
 var pX;
 var pY;
 
+var sideNum = document.getElementById("sideNum");
+var r = document.getElementById("moveAmount");
+
 var xCoords = new Array();
 var yCoords = new Array();
 
-drawPixel(x1,y1,0,0,255,255);
-drawPixel(x2,y2,0,0,255,255);
-drawPixel(x3,y3,0,0,255,255);
+document.getElementById("iterations").value = 50000;
+document.getElementById("sideNum").value = 3;
+document.getElementById("moveAmount").value = 2;
 
-document.getElementById("iterations").value = 500;
 var timesClicked = 0;
 
 function callIterate(){
+  for(var i = 0;i<Number(sideNum.value);i++){
+    var angle = interiorAngle(Number(sideNum.value)) * (i+1);
+    if(sideNum.value == 3){
+      angle += 30;
+    }
+    if(sideNum.value == 5){
+      angle += 18;
+    }
+    xCorners[i] = mapRange(Math.cos(angle*(Math.PI/180))*400,-400,400,0,(800/(r.value-1)));
+    yCorners[i] = mapRange(Math.sin(angle*(Math.PI/180))*400,-400,400,0,(800/(r.value-1)));
+
+  }
   iterate(Number(document.getElementById("iterations").value));
   timesClicked += 1;
   display();
 }
 
+function mapRange (value, a, b, c, d) {
+    // first map value from (a..b) to (0..1)
+    value = (value - a) / (b - a);
+    // then map it from (0..1) to (c..d) and return it
+    return c + value * (d - c);
+}
+
 function reset(){
-  ctx.save();
-
-  // Use the identity matrix while clearing the canvas
-  ctx.setTransform(1, 0, 0, 1, 0, 0);
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  // Restore the transform
-  ctx.restore();
-
   for(var i = 0;i<xCoords.length;i++){
-    xCoords.length = 0;
-    yCoords.length = 0;
+    xCoords[i] = null;
+    yCoords[i] = null;
   }
+  for(var x = 0;x<800;x++){
+    for(var y = 0;y<800;y++){
+      drawPixel(x,y,0,0,0,255);
+    }
+  }
+  updateCanvas();
+}
+
+function interiorAngle(sides){
+  return 360/sides;
 }
 
 var choice;
@@ -55,55 +73,32 @@ var first = false;
 function display(){
     for(var i = 0;i<xCoords.length;i++)
     {
+      xCoords[i] = mapRange(xCoords[i],Math.min(...xCorners),Math.max(...xCorners),0,800);
+      yCoords[i] = mapRange(yCoords[i],Math.min(...xCorners),Math.max(...yCorners),0,800);
       drawPixel(Math.round(xCoords[i]),Math.round(yCoords[i]),255,0,0,255);
+
     }
 
+    console.log(Math.max(...xCorners));
     updateCanvas();
 }
 
 function iterate(numIterations){
   for(var i =0;i<numIterations;i++)
   {
-      choice = Math.floor(Math.random() * 3);
-
-      if(first == false && choice == 0)
+      choice = Math.floor(Math.random() * (Number(sideNum.value)));
+      //console.log("choice: " + choice);
+      if(first == false)
       {
-        pX = x1;
-        pY = y1;
+        pX = (xCorners[choice]);
+        pY = (yCorners[choice]);
+
         first = true;
       }
-      else if(first == false && choice == 1)
+      else
       {
-        pX = x2;
-        pY = y2;
-        first = true;
-      }
-      else if(first == false && choice == 2)
-      {
-        pX = x3;
-        pY = y3;
-        first = true;
-      }
-      else if(first == true && choice == 0)
-      {
-        pX = (pX+x1)/2;
-        pY = (pY+y1)/2;
-
-        xCoords[i] = pX;
-        yCoords[i] = pY;
-      }
-      else if(first == true && choice == 1)
-      {
-        pX = (pX+x2)/2;
-        pY = (pY+y2)/2;
-
-        xCoords[i] = pX;
-        yCoords[i] = pY;
-      }
-      else if(first == true && choice == 2)
-      {
-        pX = (pX+x3)/2;
-        pY = (pY+y3)/2;
+        pX = (pX+(xCorners[choice]))/Number(r.value);
+        pY = (pY+(yCorners[choice]))/Number(r.value);
 
         xCoords[i] = pX;
         yCoords[i] = pY;
